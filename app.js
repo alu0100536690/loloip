88bc6c955d9f558df5d1c5f105a39d46f1059486
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-12-17 17:30:00 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var path = require('path');
+var express = require('express');
+var app = express();
+
+
+var privateKey  = fs.readFileSync('./tls/key.pem', 'utf8');
+var certificate = fs.readFileSync('./tls/cert.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+
+app.use(express.static(path.join(__dirname,'gh-pages')));
+app.get('/', function(request, response) {
+ response.send('index');
+});
+
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(443);
